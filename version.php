ee34e371f35d655b7cc656490843fe79f67633cc
yiy1one <yiy1one@163.com>
2017-08-13 09:36:21 +0800

    修改版本

--- version.php
+++ version.php
@@ -3,5 +3,5 @@ if(!defined('NIU_VERSION')) {
 	define('NIU_VERSION', '正式版1.11');
 	define('NIU_VER_DATE', '2017-8-12');
 	define('NIU_RELEASE', '20170812');
-    define('USER', 'yiy2one');
+    define('USER', 'yiy1one');
 }
